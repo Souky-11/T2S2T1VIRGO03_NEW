7bbfcdf9f156131b35f443882145e084cbdc98d8
Althea To <althea.to@student.tafesa.edu.au>
2024-09-12 14:39:20 +0930

    committing all changes to the release branch

--- UniversalCalculator/MainMenu.xaml.cs
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +27,38 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void Heading_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void MathsCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			//NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+			MainPage mainPage = new MainPage();
+			this.Content = mainPage;
+		}
+
+		private void MortgageCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			MortgageCalculator mortgageCalculator = new MortgageCalculator();
+			this.Content = mortgageCalculator;
+		}
+
+		private void CurrencyConverterButton_Click(object sender, RoutedEventArgs e)
+		{
+			CurrencyConversionCalculator calculator = new CurrencyConversionCalculator();
+			this.Content = calculator;
+		}
+
+		private async void TripCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+			var dialogMessage = new MessageDialog("Trip calculator C# code will be developed later");
+			await dialogMessage.ShowAsync();
+
+		}
+
+		
 	}
 }
